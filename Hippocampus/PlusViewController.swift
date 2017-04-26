ce848b3e5f73ae6975ce2503a7f46227913d3462
EileenKuo <ekuo0917@gamil.com>
2017-04-26 18:05:12 +0800

    +: finish Weather

--- Hippocampus/PlusViewController.swift
+++ Hippocampus/PlusViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
-class PlusViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PlusViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,  UIPickerViewDelegate, UIPickerViewDataSource  {
+    
+    
+    let toolBar = UIToolbar()
     
    
+    
 //////////////////////////////////////////////////////
     
     //Date
@@ -25,12 +29,12 @@ class PlusViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         DatePicker.datePickerMode = .dateAndTime
         DatePicker.addTarget(self, action: #selector(updateDateForTextField(isDoneBtnClicked:)), for: .valueChanged)
         
-        let toolBar = UIToolbar()
+        
         toolBar.barStyle = UIBarStyle.default
         toolBar.isTranslucent = true
         
-        let donebtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneClicked))
-        let cancelbtn = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: #selector(cancelClicked))
+        let donebtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneDateClicked))
+        let cancelbtn = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: #selector(cancelDateClicked))
         let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         
         toolBar.setItems([cancelbtn, flexible, donebtn], animated: false)
@@ -39,13 +43,13 @@ class PlusViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         DateBtn.inputView = DatePicker
     }
     
-    func doneClicked() {
+    func doneDateClicked() {
         
         updateDateForTextField(isDoneBtnClicked: true)
         self.view.endEditing(true)
     }
     
-    func cancelClicked() {
+    func cancelDateClicked() {
         
         updateDateForTextField(isDoneBtnClicked: false)
         self.view.endEditing(true)
@@ -54,7 +58,8 @@ class PlusViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     func updateDateForTextField(isDoneBtnClicked: Bool) {
         
         EnterDairy.text = dateFormatter.string(from: DatePicker.date)
-        Diary_Date = DatePicker.date
+        DateBtn.text = dateFormatter.string(from: DatePicker.date)
+        //Diary_Date = DatePicker.date
     }
 
 //////////////////////////////////////////////////////
@@ -86,15 +91,72 @@ class PlusViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         myImage.image = image
         dismiss(animated: true, completion: nil)
     }
+    
+//////////////////////////////////////////////////////
+    
+    // weather
+    
+    @IBOutlet weak var weatherbtn: UITextField!
+    let weatherPicker = UIPickerView()
+    let weather = ["Sunny" , "Cloudy" , "Windy" , "Rainy" , "Snow" , "Flog"]
+    
+    func createWeather(){
+        
+        weatherPicker.delegate = self as? UIPickerViewDelegate
+        weatherPicker.dataSource = self as? UIPickerViewDataSource
+        weatherbtn.tag = 100
+        
+        let donebtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneWClicked))
+        let cancelbtn = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: #selector(cancelWClicked))
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolBar.setItems([cancelbtn, flexible, donebtn], animated: false)
+        
+        weatherbtn.inputAccessoryView = toolBar
+        weatherbtn.inputView = weatherPicker
+        
+    }
+    
+    func doneWClicked() {
+        
+        updateWeatherForTextField(isDoneBtnClicked: true)
+        self.view.endEditing(true)
+    }
+    
+    func cancelWClicked() {
+        
+        updateWeatherForTextField(isDoneBtnClicked: false)
+        self.view.endEditing(true)
+    }
+    
+    func updateWeatherForTextField(isDoneBtnClicked: Bool) {
+        
+        //weatherbtn.text = dateFormatter.string(from: DatePicker.date)
+        //Diary_Date = DatePicker.date
+    }
+    
+    // UIPickerViewDataSource 必須實作的方法：UIPickerView 有幾列可以選擇
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // UIPickerViewDataSource 必須實作的方法：UIPickerView 各列有多少行資料
+    func pickerView(_ pickerView: UIPickerView,numberOfRowsInComponent component: Int) -> Int {
+        return weather.count
+    }
+    
+    // UIPickerView 每個選項顯示的資料
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return weather[row]
+    }
+    
+    // UIPickerView 改變選擇後執行的動作
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        weatherbtn.text = weather[row]
+    }
 
     
-    
-    
-    
  
-    
-    
-    
 //////////////////////////////////////////////////////
     
     override func viewDidLoad() {
@@ -102,6 +164,8 @@ class PlusViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
         createDate()
+        
+        createWeather()
         
     }
 
